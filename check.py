b77b83680f5efecd95018b706ebe41d4d3add17f
guyao <guxuan.mojang@outlook.com>
2024-05-10 23:58:59 +0800

    可以直接选择文件，不用放进文件夹

--- check.py
+++ check.py
@@ -24,15 +24,19 @@ logging.basicConfig(
     format='[%(levelname)s] - %(message)s',
 )
 
+print("")
 print("感谢使用TextRecogn项目，原作者为@fslongjin，贡献者@zhuweihao12138，github地址https://github.com/fslongjin/TextRecogn.")
+print("")
 print("本版本为顾瑶版，贡献者@Guyao146，github地址https://github.com/Guyao146/TextRecognAIGC-COPY")
+print("")
 print("日志仅保存在本地用于诊断脚本问题，不上传至服务器，请放心使用")
+print("")
 
 logging.info('感谢使用TextRecogn项目，原作者为@fslongjin，贡献者@zhuweihao12138，github地址https://github.com/fslongjin/TextRecogn')
 logging.info('本版本为顾瑶版，贡献者@Guyao146，github地址https://github.com/Guyao146/TextRecognAIGC-COPY')
 logging.info('本日志仅保存在本地用于诊断脚本问题，不上传至服务器，请放心使用')
 
-URL = "http://47.113.179.107:8000/ai_check"
+URL = "http://location:8000/ai_check"
 
 def select_file():
     root = tk.Tk()
@@ -50,6 +54,7 @@ with open(file_path, "rb") as f:
     if response.status_code != 200:
         logging.error(f"网络请求失败，状态码{response.status_code} 返回信息{response.text}")
         print(f"网络请求失败，状态码{response.status_code} 返回信息{response.text}")
+        print("")
     else:
         # 将结果保存到 output_dir
         output_file = os.path.join(output_dir, os.path.basename(file_path))
@@ -58,3 +63,4 @@ with open(file_path, "rb") as f:
 
         logging.info(f"处理结束，结果已保存到 {output_file}")
         print(f"处理结束，结果已保存到 {output_file}")
+        print("")
